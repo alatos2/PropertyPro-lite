a2ede02c87f6614ead4072d6a4ba1e7d7b7991e6
alatos2 <alabitosin60@gmail.com>
2019-07-12 02:32:18 +0100

    feat(signinDB):set up sign in endpoint for postgres db [Delivers #167243915]

--- server/tests/login.test.js
+++ server/tests/login.test.js
@@ -50,7 +50,7 @@ describe('Login Controller', () => {
         })
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
-        .expect(400)
+        .expect(401)
         .end((err, res) => {
           if (err) throw err;
           else {
@@ -101,8 +101,8 @@ describe('Login Controller', () => {
       request(server)
         .post('/api/v1/auth/signin')
         .send({
-          email: 'drogba@gmail.com',
-          password: 'didier321',
+          email: 'dehinde@gmail.com',
+          password: 'Dehinde321',
         })
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
