f59885ed390fc343100b661fc10ddf297804cae6
alatos2 <alabitosin60@gmail.com>
2019-07-15 13:09:01 +0100

    bug(fixAuthentication):work on authentication [Fixes #167280925]

--- server/middlewares/verifications.js
+++ server/middlewares/verifications.js
@@ -3,48 +3,25 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// const { SECRET } = process.env;
+const { SECRET } = process.env;
 
-// const authentication = (req, res, next) => {
-//   try {
-//     const header = req.headers.authorization || req.headers.token || req.header('token');
-    
-//     if (!header || header === '') return res.status(401).json({ status: 401, error: 'Authentication failed' });
-
-//     const token = jwt.verify(header, SECRET);
-
-//     req.decode = token;
-//     next();
-//   } catch (e) {
-//     return res.status(401).json({ status: 401, error: 'Invalid token!' });
-//   }
-// };
-
-// export default authentication;
-
-const jwtSecret = process.env.SECRET;
-
-const authenticate = (req, res, next) => {
+const authentication = (req, res, next) => {
   try {
-    const token = req.header('token') || req.headers.authorization;
+    // const header = req.headers.authorization || req.headers.token || req.header('token');
 
-    if (!token) return res.status(401).json({ status: 401, error: 'No token provided' });
+    const header = req.headers.token || req.body.token || req.headers.authorization;
 
-    req.decode = jwt.verify(token, jwtSecret);
+    if (!header || header === '') return res.status(401).json({ status: 401, error: 'Authentication failed' });
+
+    const token = jwt.verify(header, SECRET);
+
+    req.decode = token;
+
     next();
-  } catch (error) {
-    return res.status(401).json({ status: 401, error: 'Invalid token' });
+  } catch (e) {
+    return res.status(401).json({ status: 401, error: 'Invalid token!' });
   }
 };
 
-const isAdmin = (req, res, next) => {
-  if (req.decode.is_admin === false) return res.status(401).json({ status: 401, error: 'Invalid token!' });
-  console.log(req.decode);
-  next();
-};
+export default authentication;
 
-const verification = {
-  isAdmin,
-  authenticate,
-};
-export default verification;
--- server/routes/PropertyRoute.js
+++ server/routes/PropertyRoute.js
@@ -3,17 +3,17 @@ import createPropertyAd from '../controllers/CreatePropertyAdController';
 import modifyPropertyData from '../controllers/ModifyPropertyDataController';
 import getAll from '../controllers/GetPropertyAdController';
 import deletePropertyAdvert from '../controllers/DeletePropertyController';
-import verification from '../middlewares/verifications';
-// import { isAdmin } from '../middlewares/permission';
+import authentication from '../middlewares/verifications';
+import { isAdmin } from '../middlewares/permission';
 
 const propertyRoute = express.Router();
 
 propertyRoute.get('/property', getAll.allPropertyAdverts);
 propertyRoute.get('/property/:id', getAll.specificPropertyAdvert);
 propertyRoute.get('/property-specific/?', getAll.specificPropertyAdvertType);
-propertyRoute.post('/property', verification.authenticate, verification.isAdmin, createPropertyAd);
-// propertyRoute.patch('/property/:id', authentication, modifyPropertyData.updatePropertyData);
-// propertyRoute.patch('/property/:id/sold', authentication, modifyPropertyData.markPropertyData);
-// propertyRoute.delete('/property/:id', authentication, deletePropertyAdvert);
+propertyRoute.post('/property', authentication, isAdmin, createPropertyAd);
+propertyRoute.patch('/property/:id', authentication, modifyPropertyData.updatePropertyData);
+propertyRoute.patch('/property/:id/sold', authentication, modifyPropertyData.markPropertyData);
+propertyRoute.delete('/property/:id', authentication, deletePropertyAdvert);
 
 export default propertyRoute;
