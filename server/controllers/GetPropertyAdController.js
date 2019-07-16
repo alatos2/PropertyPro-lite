afded1db6bef7311aadb473eaa54af23f982a924
alatos2 <alabitosin60@gmail.com>
2019-07-16 20:30:57 +0100

    bug(fixEndpoint): Adjusting endpoint to work on auto gradr [Fixes #167280925]

--- server/controllers/GetPropertyAdController.js
+++ server/controllers/GetPropertyAdController.js
@@ -66,7 +66,7 @@ const allPropertyAdverts = (req, res) => {
         return res.status(200).json({
           status: 200,
           data: [{
-            owner_email: properties.email,
+            owner_email: properties.owner_email,
           }],
         });
       });
