4eb5b6f5289b46e8bfbc567ce3bd6c823d950d21
alatos2 <alabitosin60@gmail.com>
2019-07-16 14:14:40 +0100

    bug(fixEndpoint): Adjusting endpoint to work on auto gradr [Fixes #167280925]

--- server/controllers/CreatePropertyAdController.js
+++ server/controllers/CreatePropertyAdController.js
@@ -21,41 +21,34 @@ const createPropertyAd = (req, res) => {
       id, email, is_admin,
     } = req.decode;
 
-    // const result = validations.validateCreatePropertyAd(req.body);
+    const result = validations.validateCreatePropertyAd(req.body);
 
-    // if (result.error) {
-    //   const errorMessage = result.error.details[0].message;
+    if (result.error) {
+      const errorMessage = result.error.details[0].message;
 
-    //   return res.status(400).json({
-    //     status: 400,
-    //     error: errorMessage.replace(/[^a-zA-Z ]/g, ''),
-    //   });
-    // }
+      return res.status(400).json({
+        status: 400,
+        error: errorMessage.replace(/[^a-zA-Z ]/g, ''),
+      });
+    }
 
-    // const propertyData = {
-    //   owner: id,
-    //   status: 'available',
-    //   price,
-    //   state,
-    //   city,
-    //   address,
-    //   type,
-    //   created_on: moment().format(),
-    //   image_url,
-    // };
+    console.log(req.body);
 
     const propertyData = {
-      type,
-      address,
-      city,
-      state,
+      owner: id,
+      status: 'available',
       price,
+      state,
+      city,
+      address,
+      type,
+      created_on: moment().format(),
       image_url,
     };
 
     pool.connect((err, client, done) => {
       client.query(addProperty(propertyData), (error, result) => {
-        // done();
+        done();
         if (error) {
           if (error.code === '23505') {
             res.status(400).json({
@@ -64,11 +57,8 @@ const createPropertyAd = (req, res) => {
             });
           }
         }
-        console.log(req.body);
-        console.log(result.rows[0]);
-        const property = result.rows[0];
-
-        console.log(property);
+        
+        // const property = result.rows[0];
 
         // return res.status(201).json({
         //   status: 201,
@@ -86,14 +76,14 @@ const createPropertyAd = (req, res) => {
         return res.status(201).json({
           status: 201,
           data: {
-            id: property.id,
-            status: property.status,
-            type: property.type,
-            state: property.state,
-            city: property.city,
-            address: property.address,
-            price: property.price,
-            created_on: property.created_on,
+            id: propertyData.id,
+            status: propertyData.status,
+            type: propertyData.type,
+            state: propertyData.state,
+            city: propertyData.city,
+            address: propertyData.address,
+            price: propertyData.price,
+            created_on: propertyData.created_on,
             image_url: propertyData.image_url,
           },
         });
